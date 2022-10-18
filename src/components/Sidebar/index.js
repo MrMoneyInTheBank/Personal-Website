d3ee53bdd9fdb026cc673b4b88fc1192f0e11ff5
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2022-10-18 16:56:47 -0400

    added social links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
     <div className="nav-bar">
         <Link className="logo" to="/">
@@ -21,6 +22,35 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="4d4d4d" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/anshuman-singh-3b2496142/"
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/MrMoneyInTheBank"
+                >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.instagram.com/theanshulad/"
+                >
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
