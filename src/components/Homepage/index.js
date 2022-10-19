4f706c9c2a014b624b4be1f3246a9879a8e46ce2
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2022-10-19 02:27:14 -0400

    animated intro/text hover bug

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,17 +1,43 @@
-import LogoTitle from '../../assets/images/logo-s.png';
-import { Link } from 'react-router-dom';
-import './index.scss';
+// import LogoTitle from '../../assets/images/logo-s.png';
+// import { Link } from 'react-router-dom';
+// import './index.scss';
+// import AnimatedLetters from '../AnimatedLetters';
+// import { useEffect, useState } from 'react';
+import {useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters';
+import LogoTitle from '../../assets/images/logo-s.png'
+// import Logo from './Logo'
+import './index.scss'
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['n', 's', 'h', ',']
+    const jobArray = ['A', 'n', ' ', 'I', 'n', 't', 'e', 'r', 'e', 's', 't', 'i', 'n', 'g', ' ', 'p', 'e', 'r', 's', 'o', 'n', '.']
+
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //       setLetterClass('text-animate-hover')
+    //     }, 4000)
+    //   }, [])
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi <br /> I'm
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i</span> 
+                <br /> 
+                <span className={`${letterClass} _13`}>I</span> 
+                <span className={`${letterClass} _14`}>'m</span> 
                 <img src={LogoTitle} alt="developer" />
-                nsh,
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}/>
                 <br />
-                An interesting person.
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22} />
                 </h1>
                 <h2>Quantitative Researcher / Mathematician / Software Engineer</h2> 
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
