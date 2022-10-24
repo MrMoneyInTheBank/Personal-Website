497ac68438f45867c3096b812d2d4d810287152b
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2022-10-24 04:28:46 -0400

    added contact page skeleton

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,7 @@
 import { faPython, faGolang, faReact, faRust, faJsSquare, faGitAlt } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 // import CPP from '../About/svg-icons/cplusplus.svg'
 // import TF from '../About/svg-icons/tensorflow.svg'
@@ -18,52 +19,55 @@ const About = () => {
 
 
     return (
-        <div className="container about-page">
-            <div className="text-zone">
-                <h1>
-                    <AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                    idx={15}
-                    />
-                </h1>
-                <p>
-                    I'm a mathematician and data scientist specializing in statistical analysis and machine learning.
-                    I have experience with statistical modelling, fullstack development, backend development, and deep learning.
-                    I'm currently looking to expand my skills as a quantitative researcher and developer, and a machine learning engineer.
-                </p>
-                <p>
-                    Within statistics, I specialize in modelling stochastic processes and its applications.
-                    Within ML, I specialize in sentiment analysis, deep learning models, and CT classification.
-                </p>
-                <p>
-                    I am insatiably curious and always learning. Outside tech, I am an avid chess player and speedcuber.
-                    After mathematics, my favorite thing in the whole wide world is ice cream. 
-                </p>
-            </div>
-            <div className="stage-cube-cont">
-                <div className="cubespinner">
-                    <div className="face1">
-                        <FontAwesomeIcon icon={faPython} color="#FFD343" />
-                    </div>
-                    <div className="face2">
-                        <FontAwesomeIcon icon={faRust} color="#F06529" />
-                    </div>
-                    <div className="face3">
-                        <FontAwesomeIcon icon={faGolang} color="#28A4D9" />
-                    </div>
-                    <div className="face4">
-                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                    </div>
-                    <div className="face5">
-                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-                    </div>
-                    <div className="face6">
-                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        <>
+            <div className="container about-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                        idx={15}
+                        />
+                    </h1>
+                    <p>
+                        I'm a mathematician and data scientist specializing in statistical analysis and machine learning.
+                        I have experience with statistical modelling, fullstack development, backend development, and deep learning.
+                        I'm currently looking to expand my skills as a quantitative researcher and developer, and a machine learning engineer.
+                    </p>
+                    <p>
+                        Within statistics, I specialize in modelling stochastic processes and its applications.
+                        Within ML, I specialize in sentiment analysis, deep learning models, and CT classification.
+                    </p>
+                    <p>
+                        I am insatiably curious and always learning. Outside tech, I am an avid chess player and speedcuber.
+                        After mathematics, my favorite thing in the whole wide world is ice cream. 
+                    </p>
+                </div>
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faPython} color="#FFD343" />
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faRust} color="#F06529" />
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faGolang} color="#28A4D9" />
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 }
 
--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -6,6 +6,7 @@ import LogoTitle from '../../assets/images/logo-s.png';
 
 import './index.scss'
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate-hover')
@@ -20,28 +21,31 @@ const Home = () => {
     }, [])
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i</span> 
-                <br /> 
-                <span className={`${letterClass} _13`}>I</span> 
-                <span className={`${letterClass} _14`}>'m</span> 
-                <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15}/>
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                idx={19} />
-                </h1>
-                <h2>Quantitative Researcher / Mathematician / Software Engineer</h2> 
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i</span> 
+                    <br /> 
+                    <span className={`${letterClass} _13`}>I</span> 
+                    <span className={`${letterClass} _14`}>'m</span> 
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={15}/>
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    idx={19} />
+                    </h1>
+                    <h2>Quantitative Researcher / Mathematician / Software Engineer</h2> 
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+                <Logo />
             </div>
-            <Logo />
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 }
 
