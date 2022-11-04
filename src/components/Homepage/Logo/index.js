174dce972f7cd6c790ab573e7597b6f2481b80eb
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2022-11-03 22:35:03 -0400

    uncommented gsap plugins

--- src/components/Homepage/Logo/index.js
+++ src/components/Homepage/Logo/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import LogoSa from '../../../assets/images/logo-s-1.png'
 import { useEffect, useRef } from 'react'
-// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import gsap from 'gsap'
 
 const Logo = () => {
@@ -10,17 +10,19 @@ const Logo = () => {
   const solidLogoRef = useRef()
 
   useEffect(() => {
-    // gsap.registerPlugin(DrawSVGPlugin)
+    gsap.registerPlugin(DrawSVGPlugin)
 
-    gsap.timeline().to(bgRef.current, {
-      duration: 1,
-      opacity: 1,
-    })
+    gsap
+      .timeline()
+      .to(bgRef.current, {
+        duration: 1,
+        opacity: 1,
+      })
 
-    // .from(outlineLogoRef.current, {
-    //   drawSVG: 1,
-    //   duration: 20,
-    // })
+      .from(outlineLogoRef.current, {
+        drawSVG: 1,
+        duration: 20,
+      })
 
     gsap.fromTo(
       solidLogoRef.current,
